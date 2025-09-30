8162c6a3197738ab953a402d477e6d48de8cbea7
ErickMelendez25 <a20213712@pucp.edu.pe>
2025-09-30 14:58:11 -0500

    Actualización: cambios en configuración y scripts SQL

--- server.js
+++ server.js
@@ -145,7 +145,7 @@ const __dirname = path.resolve();  // Obtener la ruta del directorio actual (cor
 
 // Configura CORS para permitir solicitudes solo desde tu frontend
 const corsOptions = {
-  origin: ['https://qiskit-production.up.railway.app', 'http://localhost:5173', 'https://qiskit.academionlinegpt.com','http://localhost:5000','http://localhost:8000'],
+  origin: ['https://qiskit-production.up.railway.app', 'http://localhost:5173', 'https://zonas.grupo-digital-nextri.com','http://localhost:5000','http://localhost:8000'],
   methods: 'GET, POST, PUT, DELETE',
   allowedHeaders: 'Content-Type, Authorization',
 };
